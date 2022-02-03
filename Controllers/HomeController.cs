00f468958cb20cbd592b6d7980db973ad7070f5a
Buscompany21 <buscobrian1@gmail.com>
2022-02-02 18:33:53 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission_4_Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            _blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieResponse ar)
+        {
+            _blahContext.Add(ar);
+            _blahContext.SaveChanges();
+            return View("MovieConfirmation", ar);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
